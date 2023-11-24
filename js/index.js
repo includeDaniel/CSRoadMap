8d19f3485f109de941bdef63abfdee3f1060d008
Felipe Araujo <141876774+FelipeGA02@users.noreply.github.com>
2023-11-24 13:01:21 -0300

    ChangeTheme

--- js/index.js
+++ js/index.js
@@ -6,21 +6,21 @@ document.getElementById("Math").onclick = () => {
     if(counter1 % 2 == 0) {
         document.getElementsByClassName("MainContent")[0].classList.remove("None")
         document.getElementsByClassName("MathSubLearningRoads")[0].classList.remove("None")
-        document.getElementById("Math").style.backgroundColor = "violet"
-        document.getElementById("Math").style.color = "white"
+        document.getElementById("Math").style.backgroundColor = "var(--darker-color)"
+        document.getElementById("Math").style.color = "var(--bgcolor)"
 
         if(counter2 % 2 != 0) {
             document.getElementsByClassName("CodeSubLearningRoads")[0].classList.add("None")
-            document.getElementById("Code").style.backgroundColor = "white"
-            document.getElementById("Code").style.color = "black"
+            document.getElementById("Code").style.backgroundColor = "var(--ligther-color)"
+            document.getElementById("Code").style.color = "var(--contrast-color)"
             counter2++
         }
         counter1++
     } else {
         document.getElementsByClassName("MainContent")[0].classList.add("None")
         document.getElementsByClassName("MathSubLearningRoads")[0].classList.add("None")
-        document.getElementById("Math").style.backgroundColor = "white"
-        document.getElementById("Math").style.color = "black"
+        document.getElementById("Math").style.backgroundColor = "var(--ligther-color)"
+        document.getElementById("Math").style.color = "var(--contrast-color)"
         counter1++
     }
 }
@@ -29,21 +29,21 @@ document.getElementById("Code").onclick = () => {
     if(counter2 % 2 == 0) {
         document.getElementsByClassName("MainContent")[0].classList.remove("None")
         document.getElementsByClassName("CodeSubLearningRoads")[0].classList.remove("None")
-        document.getElementById("Code").style.backgroundColor = "violet"
-        document.getElementById("Code").style.color = "white"
+        document.getElementById("Code").style.backgroundColor = "var(--darker-color)"
+        document.getElementById("Code").style.color = "var(--bgcolor)"
 
         if(counter1 % 2 != 0) {
             document.getElementsByClassName("MathSubLearningRoads")[0].classList.add("None")
-            document.getElementById("Math").style.backgroundColor = "white"
-            document.getElementById("Math").style.color = "black"
+            document.getElementById("Math").style.backgroundColor = "var(--ligther-color)"
+            document.getElementById("Math").style.color = "var(--contrast-color)"
             counter1++
         }
         counter2++
     } else {
         document.getElementsByClassName("MainContent")[0].classList.add("None")
         document.getElementsByClassName("CodeSubLearningRoads")[0].classList.add("None")
-        document.getElementById("Code").style.backgroundColor = "white"
-        document.getElementById("Code").style.color = "black"
+        document.getElementById("Code").style.backgroundColor = "var(--ligther-color)"
+        document.getElementById("Code").style.color = "var(--contrast-color)"
         counter2++
     }
 }
@@ -52,15 +52,15 @@ document.getElementById("SubWebFront").onclick = () => {
     if(counter3 % 2 == 0) {
         document.getElementsByClassName("WebFrontCards")[0].classList.add("Grid")
         document.getElementsByClassName("CardLine")[0].classList.remove("None")
-        document.getElementById("SubWebFront").style.backgroundColor = "violet"
-        document.getElementById("SubWebFront").style.color = "white"
+        document.getElementById("SubWebFront").style.backgroundColor = "var(--darker-color)"
+        document.getElementById("SubWebFront").style.color = "var(--bgcolor)"
 
         counter3++
     } else {
         document.getElementsByClassName("WebFrontCards")[0].classList.remove("Grid")
         document.getElementsByClassName("CardLine")[0].classList.add("None")
-        document.getElementById("SubWebFront").style.backgroundColor = "white"
-        document.getElementById("SubWebFront").style.color = "black"
+        document.getElementById("SubWebFront").style.backgroundColor = "var(--ligther-color)"
+        document.getElementById("SubWebFront").style.color = "var(--contrast-color)"
         counter3++
     }
 }
