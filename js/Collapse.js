97c9dd49ed93a2a598de4e5e445f76bd49a87f6f
Daniel Silva <danielzcomputacao@gmail.com>
2023-11-26 16:24:35 -0300

    Fixing the Collapse form and closebutton problem

--- js/Collapse.js
+++ js/Collapse.js
@@ -1,5 +1,14 @@
-document.getElementById("Html").onclick = () =>{
-    document.getElementsByClassName("ModalContainer")[0].classList.remove("None");
+document.getElementById("Html").onclick = () => {
+    document
+        .getElementsByClassName("ModalContainer")[0]
+        .classList.remove("None");
     document.getElementsByClassName("ModalContainer")[0].classList.add("Flex");
-    document.body.style.overflow = "hidden"
-}
+    document.body.style.overflow = "hidden";
+};
+document.getElementById("CloseButton").onclick = () => {
+    document
+        .getElementsByClassName("ModalContainer")[0]
+        .classList.remove("Flex");
+    document.getElementsByClassName("ModalContainer")[0].classList.add("None");
+    document.body.style.overflow = "visible";
+};
