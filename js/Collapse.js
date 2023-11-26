511c476e00b80e3ca321d2e8138036acaeca17ee
Felipe Araujo <141876774+FelipeGA02@users.noreply.github.com>
2023-11-26 16:03:55 -0300

    Z-indexCorrection

--- js/Collapse.js
+++ js/Collapse.js
@@ -1,6 +1,5 @@
 document.getElementById("Html").onclick = () =>{
     document.getElementsByClassName("ModalContainer")[0].classList.remove("None");
     document.getElementsByClassName("ModalContainer")[0].classList.add("Flex");
-    document.getElementsByClassName("WebFrontCards")[0].classList.remove("Grid");
     document.body.style.overflow = "hidden"
 }
